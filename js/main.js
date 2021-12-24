c1e5419effa804960335d273d8a10a961fc608bf
santosSeba <95554204+santosSeba@users.noreply.github.com>
2021-12-23 23:30:20 -0300

    Subo los cambios para la entrega
    Subo en un nuevo repositorio todos los cambios del proyecto

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const typed = new Typed(".typed", {
+    strings: [
+        "<i>Web Developer</i>", 
+        "<i>Technical Recruiter</i>", 
+        "<i>Business Administrator</i>"
+    ],
+    stringsElement: "#cadenas-texto",
+    typeSpeed: 75,
+    startDelay: 300,
+    backSpeed:75,
+    smartBackspace:true,
+    shuffle:false,
+    backDelay:1500,
+    loop:true,
+    loopCount:false,
+    showCursor:true,
+    cursorChar:"|",
+    contentType:"html"
+});
